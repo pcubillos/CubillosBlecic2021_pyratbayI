8c77558beddaf461a44b0753d42aec6fad368e63
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-05-12 15:07:17 +0200

    Updated script and files until tli runs (CH4 pending).

--- compendium.sh
+++ compendium.sh
@@ -1,6 +1,7 @@
-# Clone code
-# From the directory where this file is located, execute:
+# Define topdir (in your top working directory) to make your life easier:
 topdir=`pwd`
+
+# Clone (download) the necessary code:
 cd $topdir
 git clone --recursive https://github.com/pcubillos/pyratbay
 cd $topdir/pyratbay
@@ -31,6 +32,15 @@ rm -f *.zip
 cd $topdir/run01
 $topdir/code/pf_tips_H2O-NH3.py
 
+# Generate partition-function files for HCN and CH4:
+cd $topdir/run01
+$topdir/pyratbay/scripts/PFformat_Exomol.py \
+      $topdir/inputs/opacity/1H-12C-14N__Harris.pf \
+      $topdir/inputs/opacity/1H-13C-14N__Larner.pf
+$topdir/pyratbay/scripts/PFformat_Exomol.py \
+      $topdir/inputs/opacity/12C-1H4__YT10to10.pf
+
+
 # Compress LBL databases:
 cd $topdir/run01
 $topdir/repack/repack.py repack_H2O.cfg
@@ -38,23 +48,16 @@ $topdir/repack/repack.py repack_HCN.cfg
 $topdir/repack/repack.py repack_NH3.cfg
 $topdir/repack/repack.py repack_CH4.cfg
 
-# :::  OK  :::
-
-# Generate HCN partition function:  
-cd $topdir/run01
-$topdir/pyratbay/scripts/PFformat_Exomol.py \
-      $topdir/inputs/opacity/1H-12C-14N__Harris.pf \
-      $topdir/inputs/opacity/1H-13C-14N__Larner.pf
-
 
 # Make TLI files:
 cd $topdir/run01/
-$topdir/pyratbay/pbay.py -c tli_byte_NH3.cfg
-$topdir/pyratbay/pbay.py -c tli_exomol_HCN.cfg
 $topdir/pyratbay/pbay.py -c tli_hitemp_CO2.cfg
 $topdir/pyratbay/pbay.py -c tli_hitemp_CO.cfg
 $topdir/pyratbay/pbay.py -c tli_hitemp_H2O.cfg
-$topdir/pyratbay/pbay.py -c tli_hitran_CH4.cfg
+$topdir/pyratbay/pbay.py -c tli_exomol_HCN.cfg
+$topdir/pyratbay/pbay.py -c tli_exomol_NH3.cfg
+# :::  OK  :::
+$topdir/pyratbay/pbay.py -c tli_exomol_CH4.cfg
 
 
 # Make atmospheric files:
