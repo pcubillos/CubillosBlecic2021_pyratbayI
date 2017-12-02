56ad5989707c2d7cdadff68d80b78233336f85bc
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-12-02 15:44:57 +0100

    Added transmission data files and make_filters.py.

---
+++ code/make_filters.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import numpy as np
+
+
+def tophat(wl0, width, margin, dlambda, file=None):
+  """
+  Generate a top-hat filter transmission function.
+
+  Parameters
+  ----------
+  wl0:  Float
+     Filter central wavelength in microns.
+  width:
+     Filter width in microns.
+  margin:
+     Margin with zero-valued transmission.
+  dlambda:
+     Spectral sampling rate in microns.
+  file: String
+     Name of the output file.
+
+  Example
+  -------
+  >>> wl0     = 0.743
+  >>> width   = 0.010
+  >>> margin  = 0.003
+  >>> dlambda = 0.0002
+  >>> tophat(wl0, width, margin, dlambda, "tophat_filter.dat")
+  """
+  wl = np.arange(wl0-width/2.0-margin,
+                 wl0+width/2.0+margin, dlambda)
+  trans = np.ones(len(wl))
+  trans[np.where(np.abs(wl-wl0)>width/2.0)] = 0.0
+
+  if file is not None:
+    with open(file, "w") as f:
+      f.write("# Wavelength (um)  Transmission\n")
+      for i in np.arange(len(wl)):
+        f.write("{:10.6f}         {:6.4f}\n".format(wl[i], trans[i]))
+  return wl, trans
+
+
+if __name__ == "__main__":
+  # Gather files:
+  dpath = "../inputs/data/"
+  fdata = os.listdir(dpath)
+  for f in fdata:
+    if not f.endswith(".csv"):
+      fdata.remove(f)
+  fdata = sorted(fdata)
+
+
+  # Read data:
+  for j in np.arange(len(fdata)):
+    with open(dpath + fdata[j], "r") as f:
+      lines = f.readlines()
+    planet = fdata[j].split("_")[0]
+    nlines = len(lines)
+
+    data    = "data   = "
+    error   = "uncert = "
+    filters = "filter = "
+    k = 0
+
+    for i in np.arange(nlines):
+      if lines[i].strip() == "" or lines[i].strip().startswith("#"):
+        continue
+      elif lines[i].strip().startswith("@"):
+        inst = lines[i].strip()[1:]
+      else:
+        wl, width, rprs, runc = lines[i].split(",")
+        wl    = float(wl)
+        width = float(width) * 2.0
+        rprs  = float(rprs)
+        runc  = float(runc)
+        ffile = "../inputs/filters/{:s}_{:s}_{:5.3f}um.dat".format(planet,
+                                                                   inst, wl)
+        if (inst.startswith("stis") or inst.startswith("wfc3")   or
+            inst.startswith("acs")  or inst.startswith("nicmos") ):
+          tophat(wl, width, 0.1*width, width/500.0, ffile)
+        data  += "{:.8f}  ".format(rprs**2)
+        error += "{:.8f}  ".format(2.0*rprs*runc)
+        if (k+1) % 5 == 0 and k != 0:
+          data  += "\n         "
+          error += "\n         "
+        filters += "\n         {:s}".format(ffile)
+        k += 1
+
+    print(planet)
+    print(data)
+    print(error)
+    print(filters)
