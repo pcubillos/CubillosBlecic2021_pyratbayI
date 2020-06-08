9eacc2a77f2f5356b18fb7e57b539600cd50e5aa
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2020-06-08 17:22:19 +0200

    Updated config files for WFC3 sample.

--- code/fig_adi.py
+++ code/fig_adi.py
@@ -5,131 +5,143 @@ import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import ConfigParser as configparser
-plt.ioff()
+#plt.ioff()
 
 
-# Data files:
-folders = [
-  "../run02_HATP11b/",
-  "../run03_HATP32b/",
-  "../run04_HATP38b/",
-  "../run05_WASP043b/",
-  "../run06_WASP063b/",
-  "../run07_WASP067b/",
-  "../run08_WASP101b/",
-  "../run09_WASP107b/",
-  ]
-nplanets = len(folders)
+# Taken from stats/sample_stats.tex:
+pyrat = [
+    'run_GJ-0436b_tsiaras/MCMC_GJ-0436b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-03b_tsiaras/MCMC_HATP-03b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-17b_tsiaras/MCMC_HATP-17b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-18b_tsiaras/MCMC_HATP-18b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-32b_tsiaras/MCMC_HATP-32b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-38b_tsiaras/MCMC_HATP-38b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-41b_tsiaras/MCMC_HATP-41b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_HD-149026b_tsiaras/MCMC_HD-149026b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_K2-018b_tsiaras/MCMC_K2-018b_tsiaras_1.0-2.0um_w00000-m0.npz',
+    'run_WASP-029b_tsiaras/MCMC_WASP-029b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_WASP-043b_tsiaras/MCMC_WASP-043b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_WASP-063b_tsiaras/MCMC_WASP-063b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_WASP-067b_tsiaras/MCMC_WASP-067b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_WASP-069b_tsiaras/MCMC_WASP-069b_tsiaras_1.0-2.0um_w00000-m0.npz',
+    'run_WASP-074b_tsiaras/MCMC_WASP-074b_tsiaras_1.0-2.0um_w00000-m0.npz',
+    'run_WASP-080b_tsiaras/MCMC_WASP-080b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    'run_WASP-101b_tsiaras/MCMC_WASP-101b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    None,
+    'run_XO-1b_tsiaras/MCMC_XO-1b_tsiaras_1.0-2.0um_w00000-0c.npz',
+    ]
 
-# Tsiaras 2018:                                                #dummy
-snr_t = np.array([7.62, 14.32, 5.47, 7.34, 12.22, 5.87, 14.03, 1.0])
-adi_t = np.array([6.61, 16.44, 0.67, 1.93,  0.00, 0.27,  0.00, -1.0])
+other = [
+    None,
+    None,
+    None,
+    None,
+    'run_HATP-32b_damiano/MCMC_HATP-32b_damiano_1.0-2.0um_w00000-0c.npz',
+    'run_HATP-38b_bruno/MCMC_HATP-38b_bruno_1.0-2.0um_w00000-0c.npz',
+    None,
+    None,
+    'run_K2-018b_benneke/MCMC_K2-018b_benneke_1.0-5.5um_w00000-0c.npz',
+    None,
+    'run_WASP-043b_stevenson/MCMC_WASP-043b_stevenson_1.0-5.5um_w00000-0c.npz',
+    'run_WASP-063b_kilpatrick/MCMC_WASP-063b_kilpatrick_1.0-2.0um_w000h0-0c.npz',
+    'run_WASP-067b_bruno/MCMC_WASP-067b_bruno_1.0-2.0um_w00000-0c.npz',
+    None,
+    None,
+    None,
+    'run_WASP-101b_wakeford/MCMC_WASP-101b_wakeford_1.0-2.0um_w00000-m0.npz',
+    'run_WASP-103b_kreidberg/MCMC_WASP-103b_kreidberg_1.0-5.5um_w00000-0c.npz',
+    None,
+    ]
+
+nplanets = len(pyrat)
+
+names = [None] * nplanets
+for i in range(nplanets):
+    cfg = pyrat[i] if pyrat[i] is not None else other[i] 
+    names[i] = os.path.split(cfg)[0].split('_')[1]
 
 
 # Read flatfit results:
-with open("flatfit.dat", "r") as f:
-  lines = f.readlines()
-for i in np.arange(len(lines)):
-  if lines[i].startswith("---"):
-    break
+with open("stats/flat_fit.dat", "r") as f:
+    lines = f.readlines()
+for i in range(len(lines)):
+    if lines[i].startswith("---"):
+        break
+
 # Extract values:
-name = np.zeros(nplanets, "|S15")
-flatchisq = np.zeros(nplanets)
-flatbic   = np.zeros(nplanets)
-for j in np.arange(nplanets):
-  info = lines[i+j+1].split()
-  name[j], flatchisq[j], flatbic[j] = info[0], info[2], info[3]
+flat_bic = np.zeros([nplanets,2])
+for j in range(i+1, len(lines)):
+    info = lines[j].split()
+    idx = list(names).index(info[0])
+    ts = int(info[1] != 'tsiaras')
+    flat_bic[idx, ts] = info[4]
 
 
-bic      = np.tile(np.inf, nplanets)
-snr_pb   = np.zeros(nplanets)
-# Read PB MCMC results:
-cwd = os.getcwd() + "/"
-for j in np.arange(nplanets):
-  files = os.listdir(cwd + folders[j])
-  cfiles = []
-  for f in sorted(files)[::-1]:
-    if f.startswith("mcmc") and f.endswith(".cfg"):
-      config = configparser.SafeConfigParser()
-      config.read(cwd + folders[j] + f)
-      data  = np.array(config.get('pyrat', 'data').split(), float)
-      error = np.array(config.get('pyrat', 'uncert').split(), float)
-      if os.path.exists(cwd + folders[j] + config.get('pyrat','logfile')):
-        cfiles.append(f)
+pyrat_bic = np.tile(-np.inf, nplanets)
+for i in range(nplanets):
+    if pyrat[i] is None:
+        continue
+    with np.load(pyrat[i]) as mcmc:
+        pyrat_bic[i] = mcmc['BIC']
 
-  nruns = len(cfiles)
-  for i in np.arange(nruns):
-    config = configparser.SafeConfigParser()
-    config.read(cwd + folders[j] + cfiles[i])
-    # Extract info:
-    d = np.load(cwd + folders[j] +
-                config.get('pyrat','logfile').replace("log","npz"))
-    try:
-      if d['BIC'] < bic[j]:
-        bic[j]      = d['BIC']
-        snr_pb[j]   = np.median(data/error)
-    except:
-      pass
-    d.close()
+other_bic = np.tile(-np.inf, nplanets)
+for i in range(nplanets):
+    if other[i] is None:
+        continue
+    with np.load(other[i]) as mcmc:
+        other_bic[i] = mcmc['BIC']
 
-adi_pb = 0.5*np.clip(flatbic-bic, 0, np.inf)
+
+adi_pb = 0.5*np.clip(flat_bic[:,0]-pyrat_bic, 0, np.inf)
+adi_other = 0.5*np.clip(flat_bic[:,1]-other_bic, 0, np.inf)
+adi_ts = np.array([
+    0.0,  0.0, 0.28,  5.71, 16.44, 0.67, 7.29, 0.0, 4.7, 1.25,
+    1.93, 0.0, 0.27, 13.3,   0.0,  1.16, 0.0, np.inf,  3.15])
 
 
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Plot
 
-fs = 12
+fs = 11
 ms = 6
 dy1 = np.tile(1.04, nplanets)
 dy2 = np.tile(1.04, nplanets)
 dy1[1] = dy2[2] = dy2[6] = 0.9
 
-label = ["HAT-P-11b", "HAT-P-32b", "HAT-P-38b", "WASP-43b",
-         "WASP-63b",  "WASP-67b",  "WASP-101b", "WASP-107b"]
-
-col = ['navy', 'orangered']
-
-dadi = adi_pb - adi_t
-dadi[-1] = 0.0
-isort = np.argsort(dadi)
-
-lab = np.asarray(label)[isort]
-adit  = adi_t [isort]
-adipb = adi_pb[isort]
+col = ['navy', 'orangered', 'limegreen']
 
 
-plt.figure(1, (6.5, 4.25))
+plt.figure(1, (6.5, 5))
 plt.clf()
 plt.subplots_adjust(0.06, 0.12, 0.98, 0.97)
 ax = plt.subplot(111)
 for i in np.arange(nplanets):
-  if adit[i] >= 0:
-    tsi = plt.plot(adit[i],i, "D", color="w", ms=ms+1, mec=col[0], mew=1.5,
-                   label='Tsiaras et al. (2018)')
-  pbi = plt.plot(adipb[i], i, "o", color=col[1], ms=ms, label='This work')
-  plt.text(-1.28, i, lab[i], ha='left', va='center', fontsize=fs)
+    pbi = plt.plot(adi_pb[i], i, "o", color=col[1], ms=ms,
+        label='This work / Tsiaras')
+    tsi = plt.plot(adi_ts[i], i, "D", color=col[0], ms=ms+1, mfc='none',
+        mew=1.5, label='Tsiaras et al. (2018)')
+    oi = plt.plot(adi_other[i], i, "+", color=col[2], ms=ms+1, mfc='none',
+        mew=1.5, label='This work / Other')
+    plt.text(-1.28, i, names[i], ha='left', va='center', fontsize=fs)
 ax.set_xscale('symlog')
-plt.xlim(-1.32, 60)
-plt.ylim(-0.5, 9.2)
+plt.xlim(-1.32, 25)
+plt.ylim(18.5, -2)
 plt.gca().xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax.set_xticks([0,1,3, 10, 20, 50])
+ax.set_xticks([0, 1, 3, 10, 20])
 ax.set_yticklabels([])
-ax.set_yticks(np.arange(nplanets))
 ax.tick_params(labelsize=fs-1)
 
-ax.legend(handles=tsi+pbi, loc='upper right', fontsize=fs)
-ax.set_xlabel(r"ADI = 0.5$\,\Delta$BIC", fontsize=fs)
+ax.legend(handles=tsi+pbi+oi, loc='upper right', fontsize=fs)
+ax.set_xlabel("ADI", fontsize=fs)
 ax.set_ylabel("Planet", fontsize=fs)
 
 plt.axvline( 0, dashes=(7,2,2,2), lw=1.5, color="0.8", zorder=-10)
 plt.axvline( 3, ls="--", lw=1.5, color="0.8", zorder=-10)
 plt.axvline(11, ls="--", lw=1.5, color="0.8", zorder=-10)
-plt.text( 3, 0, r"3$\sigma$", ha='right', va='center', rotation=90,
+plt.text( 2.9, 17, r"3$\sigma$", ha='right', va='center', rotation=90,
          color="0.7", fontsize=fs)
-plt.text(11, 0, r"5$\sigma$", ha='right', va='center', rotation=90,
+plt.text(11, 17, r"5$\sigma$", ha='right', va='center', rotation=90,
          color="0.7", fontsize=fs)
-plt.savefig("../plots/adi.ps")
-plt.savefig("../plots/adi.pdf")
+plt.savefig("plots/adi.pdf")
 
---
+++ code/make_WFC3_pickles.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+import os
+import numpy as np
+
+import pyratbay as pb
+import pyratbay.io as io
+import pyratbay.tools as pt
+
+import mc3
+
+
+cfiles = [
+    'run_GJ-0436b_tsiaras/mcmc_GJ-0436b_tsiaras_w00000-mc.cfg',
+    'run_HATP-03b_tsiaras/mcmc_HATP-03b_tsiaras_w00000-mc.cfg',
+    'run_HATP-17b_tsiaras/mcmc_HATP-17b_tsiaras_w00000-mc.cfg',
+    'run_HATP-18b_tsiaras/mcmc_HATP-18b_tsiaras_w00000-mc.cfg',
+    'run_HATP-32b_damiano/mcmc_HATP-32b_damiano_w00000-mc.cfg',
+    'run_HATP-32b_tsiaras/mcmc_HATP-32b_tsiaras_w00000-mc.cfg',
+    'run_HATP-38b_bruno/mcmc_HATP-38b_bruno_w00000-mc.cfg',
+
+    'run_HATP-38b_tsiaras/mcmc_HATP-38b_tsiaras_w00000-mc.cfg',
+    'run_HATP-41b_tsiaras/mcmc_HATP-41b_tsiaras_w00000-mc.cfg',
+    'run_HD-149026b_tsiaras/mcmc_HD-149026b_tsiaras_w00000-mc.cfg',
+    'run_K2-018b_benneke/mcmc_K2-018b_benneke_wmdm00-mc.cfg',
+    'run_K2-018b_tsiaras/mcmc_K2-018b_tsiaras_w00000-mc.cfg',
+    'run_WASP-029b_tsiaras/mcmc_WASP-029b_tsiaras_w00000-mc.cfg',
+    'run_WASP-043b_stevenson/mcmc_WASP-043b_stevenson_wmdm00-mc.cfg',
+
+    'run_WASP-043b_tsiaras/mcmc_WASP-043b_tsiaras_w00000-mc.cfg',
+    'run_WASP-063b_kilpatrick/mcmc_WASP-063b_kilpatrick_w000h0-mc.cfg',
+    'run_WASP-063b_tsiaras/mcmc_WASP-063b_tsiaras_w000h0-mc.cfg',
+    'run_WASP-067b_bruno/mcmc_WASP-067b_bruno_w00000-mc.cfg',
+    'run_WASP-067b_tsiaras/mcmc_WASP-067b_tsiaras_w00000-mc.cfg',
+    'run_WASP-069b_tsiaras/mcmc_WASP-069b_tsiaras_w00000-mc.cfg',
+    'run_WASP-074b_tsiaras/mcmc_WASP-074b_tsiaras_w00000-mc.cfg',
+
+    'run_WASP-080b_tsiaras/mcmc_WASP-080b_tsiaras_w00000-mc.cfg',
+    'run_WASP-101b_tsiaras/mcmc_WASP-101b_tsiaras_w00000-mc.cfg',
+    'run_WASP-101b_wakeford/mcmc_WASP-101b_wakeford_w00000-mc.cfg',
+    'run_WASP-103b_kreidberg/mcmc_WASP-103b_kreidberg_wmdm00-mc.cfg',
+    'run_XO-1b_tsiaras/mcmc_XO-1b_tsiaras_w00000-mc.cfg',
+    ]
+ntargets = len(cfiles)
+
+for cfile in cfiles:
+    folder, cfg = os.path.split(cfile)
+    _, planet, dset = folder.split('_')
+    with pt.cd(folder):
+        pyrat = pb.run(cfg, init=True, no_logfile=True)
+        mcmc = np.load(pyrat.ret.mcmcfile)
+        post, zchain, zmask = mc3.utils.burn(mcmc)
+        pyrat.ret.posterior = post
+        # I want to evaluate less than ~1e5 unique samples:
+        nmax = int(1e5 / (6 * 0.01*mcmc['acceptance_rate']))
+        pyrat.percentile_spectrum(nmax)
+        pfile = pyrat.ret.mcmcfile.replace('.npz', '.pickle')
+        io.save_pyrat(pyrat, pfile)
+        mcmc.close()
+
